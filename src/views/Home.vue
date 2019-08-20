c18379f59c7353cd503f8b1de6f9c7fc245ca62d
Sunny <sunny@koek.nl>
2019-08-20 14:08:29 +0200

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,91 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home-view-container">
+        <h1>Adopt a new best friend</h1>
+        {{ getAllCats.length }}
+        {{ animalsCount }}
+        <button class="btn btn-primary" @click="togglePetForm">Add new pet</button>
+
+        <b-form @submit.prevent="handleSubmit" v-if="showPetForm">
+            <b-form-group id="input-group-2" label="Pet's name:" label-for="input-2">
+                <b-form-input
+                    id="input-2"
+                    type="text"
+                    v-model="formData.name"
+                    required
+                    placeholder="Enter name"
+                ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-3" label="Species:" label-for="input-3">
+                <b-form-select
+                    id="input-3"
+                    v-model="formData.species"
+                    :options="['cats', 'dogs']"
+                    required
+                ></b-form-select>
+            </b-form-group>
+
+            <b-form-group id="input-group-2" label="Pet's age:" label-for="input-2">
+                <b-form-input
+                    id="input-2"
+                    type="number"
+                    v-model="formData.age"
+                    required
+                    placeholder="Enter age"
+                ></b-form-input>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Submit</b-button>
+            <b-button type="reset" variant="danger">Reset</b-button>
+        </b-form>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    import { mapActions, mapGetters } from 'vuex'
+    export default {
+        name: 'home',
+        data() {
+            return {
+                showPetForm: false,
+                formData: {
+                    name: '',
+                    age: 0,
+                    species: null,
+                }
+            }
+        },
+        computed: {
+          ...mapGetters([
+              'animalsCount',
+              'getAllCats'
+          ])
+        },
+        methods: {
+            ...mapActions([
+               'addPet'
+            ]),
+            togglePetForm() {
+                this.showPetForm = !this.showPetForm
+            },
+            handleSubmit() {
+                const { species, age, name } = this.formData
+                const payload = {
+                    species,
+                    pet: {
+                        name,
+                        age
+                    }
+                }
+                this.addPet(payload)
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+                // reset form after submit
+                this.formData = {
+                    name: '',
+                    age: 0,
+                    species: null,
+                }
+            }
+        }
+    }
 </script>
